636f8b41fb72ea157079b6e80cf85017896bd07f
Christopher Marin <christopher.marin@gmail.com>
2024-12-21 13:16:32 -0800

    Still trying to extend width.

--- nextjs/app/(dashboard)/chatsse/page.tsx
+++ nextjs/app/(dashboard)/chatsse/page.tsx
@@ -320,7 +320,7 @@ export default function Chat() {
       <AppSidebar toggleChatbot={() => {}} />
       <main className={`flex-1 flex flex-col h-screen transition-all duration-300 ease-in-out ${sidebarOpen ? 'ml-64' : 'ml-16'}`}>
         <div className="flex-1 flex flex-col">
-          <div className="flex flex-col h-full p-4">
+          <div className="flex flex-col h-full p-0">
             {mode === 'single' ? (
               <div
                 className="grid h-full gap-4"
