3d7e71b88d808631fb72a96de107308943f39aee
Christopher Marin <christopher.marin@gmail.com>
2024-12-24 16:44:38 -0800

    Trying to fix import library.

--- fastapi/app/api/v1/chat.py
+++ fastapi/app/api/v1/chat.py
@@ -23,7 +23,8 @@ from uuid import UUID
 from llama_index.core.llms import ChatMessage as LlamaChatMessage, MessageRole
 from llama_index.llms.openai import OpenAI
 from llama_index.core.memory import ChatSummaryMemoryBuffer, ChatMemoryBuffer
-from llama_index.storage.postgresql import PostgresChatStore
+#from llama_index.storage.postgresql import PostgresChatStore
+from llama_index.storage.chat_store.postgres import PostgresChatStore
 #from llama_index.core.memory import ChatMemoryBuffer
 # ------------------------------------------------------------------------- #
 
